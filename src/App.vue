58c291283a0e5fc904e934c96e8ff722fdea4ddf
Jesus Garcia <fairplaay16@gmail.com>
2021-03-20 09:42:07 -0400

    update branch

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,16 @@
 						<v-btn color="primary" @click="dialog = true"> Nuevo producto </v-btn>
 					</v-col>
 				</v-row>
+				<v-row>
+					<v-col cols="12">
+						<v-data-table
+							:headers="headers"
+							:items="items"
+							:items-per-page="5"
+							class="elevation-1"
+						></v-data-table>
+					</v-col>
+				</v-row>
 			</v-container>
 		</v-main>
 		<v-dialog v-model="dialog" width="500">
@@ -29,6 +39,9 @@
 						label="Nombre del producto"
 						dense
 						outlined
+						:error-messages="nameErrors"
+						@input="$v.name.$touch()"
+						@blur="$v.name.$touch()"
 					></v-text-field>
 					<v-text-field
 						v-model="price"
@@ -36,6 +49,10 @@
 						dense
 						outlined
 						label="Precio"
+						placeholder="Al mayor en dolar"
+						:error-messages="priceErrors"
+						@input="$v.price.$touch()"
+						@blur="$v.price.$touch()"
 					></v-text-field>
 					<v-text-field
 						v-model="cant"
@@ -43,6 +60,19 @@
 						dense
 						outlined
 						label="Cantidad"
+						:error-messages="cantErrors"
+						@input="$v.cant.$touch()"
+						@blur="$v.cant.$touch()"
+					></v-text-field>
+					<v-text-field
+						v-model="measure"
+						type="text"
+						dense
+						outlined
+						label="Medida ej: 50gr, 1kg, 1Lts"
+						:error-messages="measureErrors"
+						@input="$v.measure.$touch()"
+						@blur="$v.measure.$touch()"
 					></v-text-field>
 				</v-card-text>
 
@@ -60,7 +90,7 @@
 
 <script>
 import { validationMixin } from 'vuelidate';
-const { required, number } = require('vuelidate/lib/validators');
+const { required, minLength, maxLength } = require('vuelidate/lib/validators');
 
 export default {
 	name: 'App',
@@ -68,9 +98,52 @@ export default {
 	data: () => ({
 		dialog: false,
 		name: '',
-		price: 0,
-		cant: 0,
+		price: null,
+		cant: null,
+		measure: null,
+		headers: [
+			{
+				text: 'Dessert (100g serving)',
+				align: 'start',
+				sortable: false,
+				value: 'name',
+			},
+			{ text: 'Calories', value: 'calories' },
+			{ text: 'Fat (g)', value: 'fat' },
+			{ text: 'Carbs (g)', value: 'carbs' },
+			{ text: 'Protein (g)', value: 'protein' },
+			{ text: 'Iron (%)', value: 'iron' },
+		],
+		items: [],
 	}),
+	computed: {
+		nameErrors() {
+			const errors = [];
+			if (!this.$v.name.$dirty) return errors;
+			!this.$v.name.required && errors.push('Campo requerido');
+			!this.$v.name.minLength && errors.push('Minimo 4 Caracteres');
+			!this.$v.name.maxLength && errors.push('Maximo 100 Caracteres');
+			return errors;
+		},
+		priceErrors() {
+			const errors = [];
+			if (!this.$v.price.$dirty) return errors;
+			!this.$v.price.required && errors.push('Campo requerido');
+			return errors;
+		},
+		cantErrors() {
+			const errors = [];
+			if (!this.$v.cant.$dirty) return errors;
+			!this.$v.cant.required && errors.push('Campo requerido');
+			return errors;
+		},
+		measureErrors() {
+			const errors = [];
+			if (!this.$v.measure.$dirty) return errors;
+			!this.$v.measure.required && errors.push('Campo requerido');
+			return errors;
+		},
+	},
 	methods: {
 		save() {
 			alert('guardando...');
@@ -79,12 +152,17 @@ export default {
 	validations: {
 		name: {
 			required,
+			minLength: minLength(4),
+			maxLength: maxLength(100),
 		},
 		price: {
-			number,
+			required,
 		},
 		cant: {
-			number,
+			required,
+		},
+		measure: {
+			required,
 		},
 	},
 };
