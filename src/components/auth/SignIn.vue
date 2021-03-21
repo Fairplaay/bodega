6bd8971623fdd91cb1f8f3320294c054d7dd964c
Jesus Garcia <fairplaay16@gmail.com>
2021-03-21 11:28:38 -0400

    auth view

---
+++ src/components/auth/SignIn.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<v-row no-gutters>
+			<v-col cols="12">
+				<v-text-field label="Correo electronico" type="text" outlined></v-text-field>
+			</v-col>
+			<v-col cols="12">
+				<v-text-field label="Contraseña" type="password" outlined></v-text-field>
+			</v-col>
+		</v-row>
+		<v-row>
+			<v-col cols="12">
+				<v-btn block rounded color="primary"> Entra </v-btn>
+			</v-col>
+		</v-row>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/components/auth/SignUp.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-row no-gutters>
+		<v-col cols="12">
+			<v-text-field label="Nombre" type="text" outlined></v-text-field>
+		</v-col>
+		<v-col cols="12">
+			<v-text-field label="Correo electronico" type="text" outlined></v-text-field>
+		</v-col>
+		<v-col cols="12">
+			<v-text-field label="Contraseña" type="password" outlined></v-text-field>
+		</v-col>
+		<v-col cols="12">
+			<v-text-field label="Repite contraseña" type="password" outlined></v-text-field>
+		</v-col>
+		<v-col cols="12">
+			<v-btn block rounded color="primary"> Registrar </v-btn>
+		</v-col>
+	</v-row>
+</template>
+
+<script>
+export default {};
+</script>
